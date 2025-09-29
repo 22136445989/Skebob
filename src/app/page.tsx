023fd6692fdbd91e5d9a62f6156e12ceed2a00c4
1234445666666 <p.k.270607@gmail.com>
2025-09-29 23:40:02 +0300

    link store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div className="text-3xl">Стартовая страница</div>;
+  return (
+    <div className="text-3xl">
+      <Link href="/store">Нажми для перехода в магазин</Link>
+    </div>
+  );
 }
