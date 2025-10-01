f3c71732e51a976e8e8ab1bcf96f42350f74112b
22136445989 <accdlyatarkova@gmail.com>
2025-10-02 01:46:15 +0300

    Cart added, minor changes

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+export default function cart() {
+  return (
+    <div>
+      <div className="text-center text-3xl">
+        <Link href="/store">Главная</Link>
+      </div>
+      <h1>Корзина</h1>
+      <button
+        className="btn btn-primary w-100 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5"
+        type="button"
+      >
+        Оформить заказ
+      </button>
+    </div>
+  );
+}
--- src/app/store/page.tsx
+++ src/app/store/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { ToastContainer, toast } from "react-toastify";
+import Link from "next/link";
 export default function store() {
-  const magazine = ["финики", "тыквенные семечки", "медовая вода"];
+  const magazine = ["финики", "тыквенные семечки", "медовая вода", "скебоб"];
 
   function handleClick(productName: string) {
     toast(`Вы купили ${productName}`);
@@ -9,16 +10,39 @@ export default function store() {
 
   return (
     <div>
-      <div className="container mx-auto ">
-        <h1 className="text-center text-3xl font-bold">магазин</h1>
-        <div>
+      <div className="container mx-auto position">
+        <div className="text-center text-3xl flex justify-between">
+          <h1 className="text-center text-3xl font-bold">магазин</h1>
+          <Link href="/cart">Корзина</Link>
+        </div>
+        <div className="grid grid-cols-4 gap-4 mt-5">
+          <img
+            className="w-full h-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIvOof9agtMsk3Ty0I1vzixA1w9omqg8fl9Q&s"
+            alt="store"
+          />
+          <img
+            className="w-full h-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIvOof9agtMsk3Ty0I1vzixA1w9omqg8fl9Q&s"
+            alt="store"
+          />
+          <img
+            className="w-full h-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIvOof9agtMsk3Ty0I1vzixA1w9omqg8fl9Q&s"
+            alt="store"
+          />
+          <img
+            className="w-full h-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIvOof9agtMsk3Ty0I1vzixA1w9omqg8fl9Q&s"
+            alt="store"
+          />
           {magazine.map((item) => {
             return (
               <div key={item}>
                 <button
                   onClick={() => handleClick(item)}
                   value={item}
-                  className="btn btn-primary m-2 w-1/6 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                  className="btn btn-primary w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 rounded"
                   type="button"
                 >
                   {item}
