201ab171efebd9c9fe0214d90ad6984b5794e288
1234445666666 <p.k.270607@gmail.com>
2025-10-01 21:21:52 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="text-3xl">
+    <div className="text-center text-3xl mt-1.5">
       <Link href="/store">Нажми для перехода в магазин</Link>
     </div>
   );
